7f07cfb38023e322189aeee7192280afa4ac6e8c
Islom Toirjonov <toirjonovislomjon3@gmail.com>
2024-04-29 10:45:43 +0500

    done

---
+++ index.js
@@ -0,0 +1,14 @@
+let oyatillo = ["oyatillo", 12, true, "sag'bon"];
+
+console.log(oyatillo);
+console.log(oyatillo.length);
+
+let rost = confirm("manzilingiz tog'rimi");
+
+if (rost) {
+
+} else {
+    oyatillo.pop();
+}
+
+console.log(oyatillo);
